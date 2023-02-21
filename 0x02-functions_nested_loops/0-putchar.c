e0e47e087fdae9d6077583d9bf15328251a66dc2
perkmaybe <tobiadeniyi123@gmail.com>
2023-02-21 06:56:40 -0800

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Prints putchar
+ *
+ * Return: Always 0 (congratulation)
+ */
+int main(void)
+{
+	char chi[] = "_putchar";
+	int i = 0;
+
+	while (chi[i] != '\0')
+	{
+		_putchar(chi[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
